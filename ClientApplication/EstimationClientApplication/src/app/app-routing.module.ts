21f243db728a2ce1aa1711c38fb5a94a61efe502
Jakub Maciejewski <jakubm55555@gmail.com>
2023-11-28 21:29:15 +0100

    Add landing page, remove of tailwindcss

---
+++ ClientApplication/EstimationClientApplication/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app-component/app.component';
+
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home` path by default
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
